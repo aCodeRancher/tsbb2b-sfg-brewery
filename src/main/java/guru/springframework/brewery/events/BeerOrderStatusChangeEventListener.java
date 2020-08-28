16343b27877e984592050a5b3dbf518ee5e3787a
helen <hhma@alumni.cpp.edu>
2020-08-28 15:36:42 -0400

    relate beer order id to beer order line.

--- src/main/java/guru/springframework/brewery/events/BeerOrderStatusChangeEventListener.java
+++ src/main/java/guru/springframework/brewery/events/BeerOrderStatusChangeEventListener.java
@@ -39,7 +39,7 @@ public class BeerOrderStatusChangeEventListener {
 
     @Async
     @EventListener
-    public void listen(BeerOrderStatusChangeEvent event){
+    public void listen(BeerOrderStatusChangeEvent event) {
         System.out.println("I got an order status change event");
         System.out.println(event);
 
